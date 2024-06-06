bdba9f4ffc9ff6b463b7a88d642fada8108871f8
Oleg Krasnikov <oleg.krasnikov@diversido.io>
2024-06-06 18:48:53 +0300

    Fix ultimateChain definition

---
+++ apps/hubble/src/ultimateChain.ts
@@ -0,0 +1,19 @@
+import { defineChain } from 'viem';
+
+export default defineChain({
+  id: 24959,
+  name: 'ultimate',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'TEST',
+    symbol: 'TEST',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-ultimate-silver-narwhal-6qksqiedjv.t.conduit.xyz/'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'https://explorerl2new-ultimate-silver-narwhal-6qksqiedjv.t.conduit.xyz/' },
+  },
+});
